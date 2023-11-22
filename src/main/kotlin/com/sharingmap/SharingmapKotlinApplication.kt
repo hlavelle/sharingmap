0ac343127c380fe904462941d27198aafbbe102a
Anastasiia Klimaeva <anklimaeva@yandex.ru>
2023-11-22 18:00:04 +0300

    add Settings

--- src/main/kotlin/com/sharingmap/SharingmapKotlinApplication.kt
+++ src/main/kotlin/com/sharingmap/SharingmapKotlinApplication.kt
@@ -2,9 +2,12 @@ package com.sharingmap
 
 import kotlinx.coroutines.runBlocking
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(basePackages = arrayOf("com.sharingmap.*"))
 class SharingmapKotlinApplication
 
 fun main(args: Array<String>): Unit = runBlocking {
--- src/main/kotlin/com/sharingmap/repositories/UserRepository.kt
+++ src/main/kotlin/com/sharingmap/repositories/UserRepository.kt
@@ -2,11 +2,7 @@ package com.sharingmap.repositories
 
 import com.sharingmap.entities.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Modifying
-import org.springframework.data.jpa.repository.Query
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Repository
-import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
 @Repository
