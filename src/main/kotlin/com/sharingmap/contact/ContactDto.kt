1e68a556614d3ea114d76a6a1c925c45a7d68faa
Anastasiia Klimaeva <anklimaeva@yandex.ru>
2023-12-14 14:55:27 +0300

    remove my attempt of making validation of enum

--- src/main/kotlin/com/sharingmap/contact/ContactDto.kt
+++ src/main/kotlin/com/sharingmap/contact/ContactDto.kt
@@ -5,22 +5,8 @@ import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Pattern
 import java.util.*
 
-data class ContactDto(
-    @field:NotBlank(message = "Contact cannot be blank")
-    val contact: String,
-    @field:NotNull(message = "Type cannot be null")
-    @field:Pattern(
-        regexp = "^(WHATSAPP|TELEGRAM|PHONE)$",
-        message = "Type must be one of: WHATSAPP, TELEGRAM, PHONE"
-    )
-    val type: TypeContact)
+data class ContactDto(val contact: String, val type: TypeContact)
 
 data class ContactGetDto(val contact: String, val type: TypeContact, val id: UUID?)
 
-data class ContactUpdateDto(
-    val contact: String?,
-    @field:Pattern(
-        regexp = "^(WHATSAPP|TELEGRAM|PHONE)$",
-        message = "Type must be one of: WHATSAPP, TELEGRAM, PHONE"
-    )
-    val type: TypeContact?)
+data class ContactUpdateDto(val contact: String?, val type: TypeContact?)
